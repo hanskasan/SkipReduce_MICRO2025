1df2881f0b27c7cba21770acce85c27211b7365f
Hans Kasan <hanskasan@kaist.ac.kr>
2025-08-12 06:23:59 +0000

    Add initialization script.

---
+++ initialize.sh
@@ -0,0 +1,19 @@
+apt-get update
+apt-get install --assume-yes vim
+python -m pip install --upgrade pip
+
+pip install 'accelerate>=0.26.0'
+pip install datasets
+pip install evaluate
+pip install scikit-learn
+pip install pillow==10.2.0
+
+# pip install torchvision==0.20.0
+# pip install torchao
+# pip install bitsandbytes
+
+apt install --assume-yes build-essential devscripts debhelper fakeroot
+
+huggingface-cli login --token <YOUR TOKEN>
+# git config --global user.email "<YOUR EMAIL>"
+# git config --global user.name "<YOUR USERNAME>"
